624aec609f80133ee249aeb57a7f7987cdaf6f51
DimNekor <117307225+DimNekor@users.noreply.github.com>
2023-03-01 19:44:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,60 @@
 
 
 double pown(double value, uint16_t n) {
-
+  double osn = value;
+  if (n == 0)
+    return 1;
+  else if (n == 1)
+    return value;
+  else {
+    while (n > 1) {
+      value *= osn;
+      n -= 1;
+    }
+  }
+  return value;
 }
 
 uint64_t fact(uint16_t n) {
-
+  uint64_t res = n;
+  while (n > 1) {
+    res = res * (n-1);
+    n -= 1;
+  }
+  return res;
 }
 
 double calcItem(double x, uint16_t n) {
-
+ return pown(x, n) / fact(n); 
 }
 
 double expn(double x, uint16_t count) {
-
+  double sum = 1;
+  for (int i = 1; i < count; i++) {
+    sum += calcItem(x, i);
+  }
+  return sum;
 }
 
 double sinn(double x, uint16_t count) {
-
+  int osn = 3;
+  int counter = 2;
+  double sum = x;
+  for (int i = 3; i < (count - 1) * 2 + osn; i += 2) {
+    sum += pown(-1, counter - 1) * pown(x, i) / fact(i);
+    std::cout << sum << std::endl;
+    counter++;
+  }
+  return sum;
 }
 
 double cosn(double x, uint16_t count) {
-
+  int osn = 2;
+  int counter = 2;
+  double sum = 1;
+  for (int i = 2; i < (count - 1) * 2 + osn; i += 2) {
+    sum += pown(-1, counter - 1) * pown(x, i) / fact(i);
+    counter++;
+  }
+  return sum;
 }
